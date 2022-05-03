c3796d94aa369dfcdf194e56ecfecb9125a13937
bwreDev <colton@bwre.tech>
2022-05-02 17:20:33 -0700

    Small adjustment to UI on smaller screen.

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className='max-w-7xl mx-auto'>
+      <main className='max-w-5xl xl:max-w-7xl mx-auto'>
         <Nav />
         <Hero />
         <ContactForm />
